ca6e7d8a4415da900e26ec7cb5c887f17fdccfb7
savan147 <11mca147@srimca.edu.in>
2018-04-29 20:47:02 +1200

    Build Model and migration
    create model vehicle, vehicleFeature and AddVehicle migration

--- Vega/Migrations/20180429083905_AddVehicle.Designer.cs
+++ Vega/Migrations/20180429083905_AddVehicle.Designer.cs
@@ -11,9 +11,10 @@ using Vega.Persistence;
 namespace Vega.Migrations
 {
     [DbContext(typeof(VegaDbContext))]
-    partial class VegaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180429083905_AddVehicle")]
+    partial class AddVehicle
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Vega/Persistence/VegaDbContext.cs
+++ Vega/Persistence/VegaDbContext.cs
@@ -9,13 +9,23 @@ namespace Vega.Persistence
 {
     public class VegaDbContext : DbContext
     {
+        public DbSet<Make> Makes { get; set; }
+
+        public DbSet<Feature> Features { get; set; }
+
+
         public VegaDbContext(DbContextOptions<VegaDbContext> options) 
             : base(options)
         {
            
         }
-        public DbSet<Make> Makes { get; set; }
-        public DbSet <Feature> Features { get; set; }
+      
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<VehicleFeature>().HasKey(vf => new { vf.VehicleId, vf.FeatureId });
+
+
+        }
     }
         
     
